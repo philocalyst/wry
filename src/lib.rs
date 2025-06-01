60d9aa0f9e195bb1e94c0a508c065294989a8e77
unknown <regime_skylark.0y@icloud.com>
2025-06-01 19:35:02 -0400

    🗑️REMOVED🗑️ the pub identifiers

--- src/lib.rs
+++ src/lib.rs
@@ -2047,7 +2047,7 @@ pub trait WebViewExtWindows {
 #[cfg(target_os = "windows")]
 impl WebViewExtWindows for WebView {
   /// Captures a preview of the WebView content as an image
-  pub fn capture_preview<F>(
+  fn capture_preview<F>(&self, image_format: CapturePreviewImageFormat, callback: F) -> Result<()>
   where
     F: FnOnce(Result<Vec<u8>>) + Send + 'static,
   {
@@ -2055,7 +2055,7 @@ impl WebViewExtWindows for WebView {
   }
 
   /// Captures a preview of the WebView content and saves it to a file
-  pub fn capture_preview_to_file<P: AsRef<std::path::Path>>(
+  fn capture_preview_to_file<P: AsRef<std::path::Path>>(
     &self,
     path: P,
     image_format: CapturePreviewImageFormat,
